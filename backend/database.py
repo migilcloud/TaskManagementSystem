22e6ff41d6ee73bffc6ec6aaea6016ef14808992
migilcloud <rosalesmiguelbengero@gmail.com>
2026-06-03 18:02:07 +0800

    Configure Railway MySQL database

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = (
-    "mysql+pymysql://root:migsql12@localhost/task_management_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:tmLqaZgjwmNiBvzyAYtWSmKcwTxNcaTn@acela.proxy.rlwy.net:46069/railway"
 )
 
 engine = create_engine(DATABASE_URL)
