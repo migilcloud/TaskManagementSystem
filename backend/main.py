70244ce9c820d64e607fe68c0de9832b4d111d99
migilcloud <rosalesmiguelbengero@gmail.com>
2026-06-03 21:29:52 +0800

    Connect frontend to deployed backend

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "https://taskmanagementsystem-production-9ab1.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
